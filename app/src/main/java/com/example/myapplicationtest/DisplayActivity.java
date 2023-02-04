ed8b0893939e72de1790d11ec289f378634afb1e
Banitongwa David <davidbanitongwa@gmail.com>
2023-02-04 22:56:01 +0100

    Add google services json

--- app/src/main/java/com/example/myapplicationtest/DisplayActivity.java
+++ app/src/main/java/com/example/myapplicationtest/DisplayActivity.java
@@ -6,25 +6,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DisplayActivity extends AppCompatActivity {
-
     private TextView fullName;
     private TextView choix;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
-
         Intent intent = getIntent();
         // get data
         Etudiant etudiant = (Etudiant) intent.getSerializableExtra("data");
-
         fullName = (TextView) findViewById(R.id.fullName);
         choix = (TextView) findViewById(R.id.choix);
-
-
         fullName.setText(etudiant.getGenre() + " " + etudiant.getNom() + " " + etudiant.getPrenom());
         choix.setText(etudiant.getChoix());
-
     }
 }
